a3dc615e80ec2d6ad27d5f139e68e6afbd68f40a
Muhammad Farhan <farhanfsd.pk@gmail.com>
2021-07-27 03:52:57 +0500

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,12 +3,12 @@
 
 
 <h1>{{ $posts->title }}</h1>
-<p>By <a href="#">{{$posts->user->name}}</a></p>
+<p>
+    By <a href="#">{{$posts->user->name}} </a> in <a href="/categories/{{$posts->category->slug}}">{{$posts->category->name}}
+</a>
+</p>
 <p>{{$posts->body}}</p>
 
-    <p>
-        {{$posts->category->name}}
-    </p>
 
 
 
